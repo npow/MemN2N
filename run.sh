4cfb318cfa00f4392d84c224d7ae4ad942dd309f
npow <nissan.pow@gmail.com>
2016-02-17 18:34:43 +0000

    remove dependency on jot. #2

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 tasks="1 2 3 4 5 6 9 10 11 12 13 14 15 16 17 18 20"
 time for task in $tasks; do
   echo "running task: $task"
-  for run in `jot 10`; do
+  for run in {1..10}; do
     python -u main.py --task $task > results/${task}_${run}.txt 2>/dev/null &
   done
   wait
